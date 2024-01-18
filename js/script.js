ace693cf4c40c87e76baacde38bc3b63d7d2c21a
jagruth <jagruth@stratbeans.com>
2024-01-18 19:10:22 +0530

    implemented sample fetching user details api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-document.getElementById("btn").onclick = function() {myFunction()};
-function myFunction() {
-    alert("Hello World!");
-}
+const username = 'jagruthvasa';
+
+const userDataUrl = `https://api.github.com/users/${username}`;
+
+fetch(userDataUrl)
+      .then(response => {
+            if (!response.ok) {
+                  throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            return response.json();
+      })
+      .then(user => {
+            document.getElementById('profileImg').src = user.avatar_url;
+            document.getElementById('bio').textContent = user.bio || 'No bio available';
+            document.getElementById('usernameLink').textContent = user.login;
+            document.getElementById('usernameLink').href = user.html_url;
+
+      })
+      .catch(error => {
+            console.error('Error:', error);
+      });
